d86e3d66beb9269d08ca7726e64c14596d84e561
Erik <kireSuomi@gmail.com>
2021-05-04 15:48:11 +0200

    added test endpoint to api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test', function (Request $request) {
+    return 'hej';
+});
+
+Route::post('/name', function (Request $request) { 
+    $name = $request->input('data');
+    return 'Hej ' . $name;
+});
