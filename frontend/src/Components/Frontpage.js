899cb34023d35da1b591ce103b57e7ebac1d471b
johannasjo <johanna.sjoestrand@gmail.com>
2021-05-19 15:17:15 +0200

    basic card layout

--- frontend/src/Components/Frontpage.js
+++ frontend/src/Components/Frontpage.js
@@ -1,15 +1,16 @@
-import { useEffect } from "react";
-import axios from "axios";
+import { useEffect } from 'react';
+import axios from 'axios';
+import ProductCard from './product-page/Product';
 
 export default function Frontpage(props) {
   useEffect(() => {
     const headers = { headers: { token: localStorage.token } };
     axios
-      .get(process.env.REACT_APP_URL + "/frontpageData", headers)
-      .then((response) => {
+      .get(process.env.REACT_APP_URL + '/frontpageData', headers)
+      .then(response => {
         console.log(response);
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
   }, []);
@@ -24,6 +25,7 @@ export default function Frontpage(props) {
       >
         Logout
       </button>
+      <ProductCard></ProductCard>
     </div>
   );
 }
