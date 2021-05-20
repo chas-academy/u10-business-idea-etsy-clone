a1424b1bbbeec5bb403aaee7f9bd61c1b1e96df6
thommie-wallin <thommie.wallin@chasacademy.se>
2021-05-20 14:53:43 +0200

    create UserSeeder and configured UserFactory

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 class UserController extends Controller
 {
-   
+
 
     protected function create()
     {
@@ -46,6 +46,6 @@ class UserController extends Controller
         } else {
             return 0;
         }
- 
+
     }
 }
