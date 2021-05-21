672487c665bcd47f8054d842c8ca6fca5d68c9b4
Oskar Findahl <oskar_findahl@hotmail.com>
2021-05-21 10:56:17 +0200

    API connection and print product names

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,43 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
-import Header from './components/Header/index'; 
-import Footer from './components/Footer/index';
+import Header from './Components/Header/index';
+import Footer from './Components/footer/index';
+import Products from './Components/Produkts/Products';
 import './App.scss';
 
-
 export default function App() {
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    getProducts();
+  });
+
+  //API connection
+  const getProducts = async () => {
+    const api_call = await fetch(`http://localhost/api/products`);
+    const data = await api_call.json();
+    console.log(data);
+    setProducts(data);
+  };
+
   //Temp
   let categories = [
-    "Clothing & Shoes",
-    "Home & Living",
-    "Wedding & Party",
-    "Toys & Entertainment",
-    "Art & Collectibles",
-    "Craft Supplies & Tools",
-  ]
+    'Clothing & Shoes',
+    'Home & Living',
+    'Wedding & Party',
+    'Toys & Entertainment',
+    'Art & Collectibles',
+    'Craft Supplies & Tools'
+  ];
+
   return (
     <React.Fragment>
       <CssBaseline />
       <Header categories={categories} />
       <Container maxWidth="md" component="main">
-        <div></div>
+        <>{products ? <Products data={products} /> : <h3>No Products to show</h3>}</>
       </Container>
       <Footer />
     </React.Fragment>
