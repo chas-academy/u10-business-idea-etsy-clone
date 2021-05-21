fa2d0efdadf33457fb5e3ea4c3ad657fe1217bea
thommie-wallin <thommie.wallin@chasacademy.se>
2021-05-21 13:49:32 +0200

    refactor main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
-import Header from './components/Header/index';
-import Footer from './components/Footer/index';
-import Products from './components/Products/Products';
+import Header from './Components/Header/index';
+import Footer from './Components/Footer/index';
+// import Products from './components/Products/Products';
 import './App.scss';
 
 export default function App() {
-  const [products, setProducts] = useState();
+  // const [products, setProducts] = useState();
 
-  useEffect(() => {
-    getProducts();
-  });
+  // useEffect(() => {
+  //   // getProducts();
+  // });
 
   //API connection
-  const getProducts = async () => {
-    const api_call = await fetch(`http://localhost/api/products`);
-    const data = await api_call.json();
-    console.log(data);
-    setProducts(data);
-  };
+  // const getProducts = async () => {
+  //   const api_call = await fetch(`http://localhost/api/products`);
+  //   const data = await api_call.json();
+  //   console.log(data);
+  //   setProducts(data);
+  // };
 
   //Temp
   let categories = [
@@ -31,16 +31,12 @@ export default function App() {
     'Art & Collectibles',
     'Craft Supplies & Tools'
   ];
-<<<<<<< HEAD
-=======
-
->>>>>>> main
   return (
     <React.Fragment>
       <CssBaseline />
       <Header categories={categories} />
       <Container maxWidth="md" component="main">
-        <>{products ? <Products data={products} /> : <h3>No Products to show</h3>}</>
+        {/* <>{products ? <Products data={products} /> : <h3>No Products to show</h3>}</> */}
       </Container>
       <Footer />
     </React.Fragment>
