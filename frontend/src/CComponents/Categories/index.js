472452bc89b4df09cc56515ebb325d8f8fd2f055
Oskar Findahl <oskar_findahl@hotmail.com>
2021-05-21 15:16:48 +0200

    rename 1/2

---
+++ frontend/src/CComponents/Categories/index.js
@@ -0,0 +1,13 @@
+function Categories(props) {
+  return (
+    <ul className="categories">
+      {props.categories.map((category, index) => (
+        <li key={index}>
+          <a href="/">{category}</a>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Categories;
