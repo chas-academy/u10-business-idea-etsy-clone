1a0e3e44fb56837b751df9cc3ec8b67e6e892522
johannasjo <johanna.sjoestrand@gmail.com>
2021-05-25 10:05:59 +0200

    midway validation

--- frontend/src/Components/AddProduct/AddProductForm.js
+++ frontend/src/Components/AddProduct/AddProductForm.js
@@ -1,34 +1,40 @@
 import React from 'react';
-import { Formik } from 'formik';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import TextField from '@material-ui/core/TextField';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import Button from '@material-ui/core/Button';
+import { TextareaAutosize } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {
     '& .MuiTextField-root': {
       margin: theme.spacing(1),
-      width: '25ch'
+      width: '50ch'
     },
     '& > *': {
       margin: theme.spacing(1)
+    },
+    descriptionField: {
+      height: '200px'
     }
   }
 }));
+
+const AddProductSchema = Yup.object().shape({
+  name: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
+  price: Yup.string().matches(/^\d+$/, 'The field should have digits only').required('Required'),
+  stock: Yup.string().matches(/^\d+$/, 'The field should have digits only').required('Required')
+});
 
 function AddProductForm() {
   const classes = useStyles();
   return (
     <Formik
       initialValues={{ name: '', price: '', stock: '', description: '', image: '', category: '' }}
-      validate={values => ({
-        name: 'Required',
-        price: 'Required',
-        stock: 'Required',
-        description: 'Required',
-        image: 'Required',
-        category: 'Required'
-      })}
+      validationSchema={AddProductSchema}
       onSubmit={(values, { setSubmitting }) => {
         setSubmitting(false);
         console.log(values);
@@ -55,7 +61,6 @@ function AddProductForm() {
             variant="outlined"
             label="Name"
           />
-
           <TextField
             error={errors.price && touched.price && errors.price}
             helperText={errors.price}
@@ -66,7 +71,6 @@ function AddProductForm() {
             variant="outlined"
             label="Price"
           />
-
           <TextField
             error={errors.stock && touched.stock && errors.stock}
             helperText={errors.stock}
@@ -77,7 +81,6 @@ function AddProductForm() {
             variant="outlined"
             label="Stock"
           />
-
           <TextField
             error={errors.description && touched.description && errors.description}
             helperText={errors.description}
@@ -87,8 +90,8 @@ function AddProductForm() {
             value={values.description}
             variant="outlined"
             label="Description"
+            className={classes.descriptionField}
           />
-
           <TextField
             error={errors.image && touched.image && errors.image}
             helperText={errors.image}
@@ -99,8 +102,23 @@ function AddProductForm() {
             variant="outlined"
             label="Image"
           />
-
-          <TextField
+          <InputLabel htmlFor="outlined-category">Category</InputLabel>
+          <Select
+            native
+            value={values.category}
+            onChange={handleChange}
+            label="Category"
+            inputProps={{
+              name: 'category',
+              id: 'outlined-age-native-simple'
+            }}
+          >
+            <option aria-label="None" value="" />
+            <option value={10}>Ten</option>
+            <option value={20}>Twenty</option>
+            <option value={30}>Thirty</option>
+          </Select>
+          {/* <TextField
             error={errors.category && touched.category && errors.category}
             helperText={errors.category}
             name="category"
@@ -109,8 +127,7 @@ function AddProductForm() {
             value={values.category}
             variant="outlined"
             label="Category"
-          />
-
+          /> */}
           <Button type="submit" disabled={isSubmitting} variant="contained" color="primary">
             Submit
           </Button>
