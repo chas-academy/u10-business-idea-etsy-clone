610ab68d556a9d0c77aa8761bfc4be5194bb7570
Jakob96 <jakob96@outlook.com>
2021-05-31 15:52:25 +0200

    fix cors and origins

--- backend/config/session.php
+++ backend/config/session.php
@@ -155,7 +155,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', '.herokuapps.com'),
+    'domain' => env('SESSION_DOMAIN', '.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
