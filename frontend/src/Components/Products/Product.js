22e6542cb62856672a8c4fc0c2c4c941de5cc235
Jakob96 <jakob96@outlook.com>
2021-06-03 21:49:13 +0200

    Add link to product

--- frontend/src/Components/Products/Product.js
+++ frontend/src/Components/Products/Product.js
@@ -1,17 +1,22 @@
 import ProductCard from '../ProductCard/Card';
+import {
+  Link,
+} from "react-router-dom";
 
 const Product = ({ product }) => {
   return (
     <>
-      <ProductCard
-        name={product.name.substr(0, 50)}
-        description={product.description}
-        picture={product.picture}
-        price={product.price}
-        currency={product.currency}
-        stock={product.stock}
-        userId={product.user_id}
-      ></ProductCard>
+      <Link to={'/product/' + product.id}>
+        <ProductCard
+          name={product.name.substr(0, 50)}
+          description={product.description}
+          picture={product.picture}
+          price={product.price}
+          currency={product.currency}
+          stock={product.stock}
+          userId={product.user_id}
+        ></ProductCard>
+      </Link>
     </>
   );
 };
