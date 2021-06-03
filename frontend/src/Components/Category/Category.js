db10ad9a8c39910ad9a8d6f2e3a28152ae63f8c5
Jakob96 <jakob96@outlook.com>
2021-06-03 16:09:11 +0200

    Add category param to getProducts

--- frontend/src/Components/Category/Category.js
+++ frontend/src/Components/Category/Category.js
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import Products from '../Products/Products';
 import api from '../../api/api';
 
 function Category(props) {
   const [category, setCategory] = useState('');
+  const [products, setProducts] = useState();
 
   useEffect(() => {
       api.getCategory(props.match.params.slug).then((res) => {
         setCategory(res.data);
       });
+
+      api.getProducts(props.match.params.slug).then((res) => {
+        setProducts(res.data);
+      });
   }, [props.match.params.slug]);
 
-    return <h1>{category}</h1>
+    return <>
+    <h1>{category}</h1>
+    {products ? <Products products={products} /> : <p>No products to show</p>}
+    </>
 }
 
 export default Category;
