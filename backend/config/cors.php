65e0c0b4c31f9b337072545635817a49f4fc39cb
Jakob96 <jakob96@outlook.com>
2021-06-03 14:32:26 +0200

    update cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://etzy.shop', 'http://u10-frontend.herokuapp.com'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
