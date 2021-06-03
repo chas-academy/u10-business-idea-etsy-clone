9ba2d08e08bd5fa28fb6da37c4746658c1645af1
Jakob96 <jakob96@outlook.com>
2021-06-03 16:56:08 +0200

    Add basic grid

--- frontend/src/Components/Products/Products.js
+++ frontend/src/Components/Products/Products.js
@@ -1,12 +1,21 @@
 import Product from './Product';
+import Grid from '@material-ui/core/Grid';
 
 const Products = ({ products }) => {
   return (
-    <>
+     <Grid 
+     container 
+     spacing={1}
+  direction="row"
+  justify="center"
+  alignItems="center"
+>
       {products.map((product, index) => (
-        <Product key={index} product={product} />
+        <Grid container item lg={3} md={6} sm={12}>
+          <Product key={index} product={product} />
+        </Grid>
       ))}
-    </>
+      </Grid>
   );
 };
 
